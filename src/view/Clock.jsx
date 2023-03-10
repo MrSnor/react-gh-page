d9bd5a0f7d7b8ec987d1c858aea4346a5e16c640
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2023-03-10 23:43:08 +0530

    feat: :sparkles: add stop/start function to the clock

--- src/view/Clock.jsx
+++ src/view/Clock.jsx
@@ -1,46 +1,52 @@
-import { React, useState } from "react";
+import { useState, useEffect } from "react";
 
-const hiMom = () => {
-  console.log("Hi mom");
-};
-
-const stopClockId = setInterval(() => {
-  const newTime = new Date().toLocaleTimeString();
-  document.getElementById("clockTime").textContent = newTime;
-}, 1000);
-
+// This is a functional component that displays a clock and a button to start/stop it.
 const Clock = () => {
+  // 'isRunning' state variable determines if the clock is running or not. It's initially set to true.
+  const [isRunning, setIsRunning] = useState(true);
+  // 'currTime' state variable holds the current time. It's initially set to the current time in locale string format.
   const [currTime, setCurrTime] = useState(new Date().toLocaleTimeString());
 
-  // update time
+  // The 'updateTime' function updates the 'currTime' state variable with the current time in locale string format.
   function updateTime() {
     const newTime = new Date().toLocaleTimeString();
     setCurrTime(newTime);
   }
 
-  // comment this to make 'Stop time' work as expected
-  const stopClockId = setInterval(updateTime, 1000);
+  // The 'useEffect' hook runs every time the value of 'isRunning' changes. If 'isRunning' is true,
+  // it sets an interval to call the 'updateTime' function every second. When the component unmounts,
+  // it clears this interval using its cleanup function.
+  useEffect(() => {
+    let stopClockId;
+    if (isRunning) {
+      stopClockId = setInterval(updateTime, 1000);
+    }
+
+    return () => {
+      clearInterval(stopClockId);
+    };
+  }, [isRunning]);
 
   return (
-    <div className="bg-[#2e4049] min-h-screen font-mono grid grid-cols-1 place-content-start md:place-content-center">
-      {/* Clock */}
+    <div className="bg-slate-700 min-h-screen font-mono grid grid-cols-1 place-content-start md:place-content-center select-none">
+      {/* This div displays the current time */}
       <div
         id="clockTime"
         className="text-7xl md:text-8xl lg:text-9xl text-white text-center py-10 font-semibold"
       >
         {currTime}
       </div>
-      {/* button for time */}
+      {/* This button toggles the value of 'isRunning', starting/stopping the clock */}
       <button
         type="button"
-        className="bg-emerald-500 p-2 rounded-md text-white font-semibold text-5xl md:text-7xl flex mx-auto active:scale-110"
+        className={`p-2 rounded-md text-white font-semibold text-5xl md:text-7xl flex mx-auto active:scale-110 transition-colors duration-500 capitalize ${
+          isRunning ? "bg-rose-500" : "bg-emerald-500"
+        }`}
         onClick={() => {
-          console.log(`stopClockId : ${stopClockId}`);
-          clearInterval(stopClockId);
-          console.log("stop clicked");
+          setIsRunning((preValue) => !preValue);
         }}
       >
-        Stop time
+        {isRunning ? "Stop" : "Start"} time
       </button>
     </div>
   );
